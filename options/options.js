4dbcd781c7743dc0daf8edde357c726045371557
Crowdbotics <team@crowdbotics.com>
2021-08-18 20:01:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "asdf_29926Identifier",
+  url: "https://asdf_29926Identifier.botics.co",
+  api: "https://asdf_29926Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
